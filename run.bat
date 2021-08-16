bd6e3d24fb601b20ff360617d26f9b307b8a7166
Amethyst-szs <Amethyst-UwU@protonmail.com>
2021-08-15 21:52:17 -0700

    Combined init.bat and run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,18 @@
 @echo off
-title SMO Project Manager
-node %0\..\bin\index.js
+
+if exist node_modules (
+  title SMO Project Manager
+  node %0\..\bin\index.js
+) else (
+  title SMO Project Manager - Initalization Tool
+  echo Installing dependencies...
+  echo Please verify you have the following installed:
+  echo 	  Node
+  echo 	  NPM
+  echo 	  Python
+
+  pause
+
+  npm install
+)
+
