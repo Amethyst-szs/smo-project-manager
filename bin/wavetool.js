00a7285d74f75cd90b126608d3378cd1df5d78b5
Amethyst-szs <Amethyst-UwU@protonmail.com>
2021-10-08 11:45:38 -0700

    Remove large bit of unused code

--- bin/wavetool.js
+++ bin/wavetool.js
@@ -80,54 +80,6 @@ module.exports = {
             UpdateConsole(`Building looping BFSTM & BFSTP...`, 0, TotalTasks);
             BuildFilesLoop(WorkingDirectory, wavobj.target, wavobj.start, wavobj.end);
 
-        } else if(wavobj.type == `Loops Auto`){
-            //Setup variables
-            let StartLoopPoint = 0;
-            let EndLoopPoint = 0;
-            let CurrentSamplePoint = 264000;
-            let Samples = [];
-            let SongLength = 0;
-            let isSearchingLoop = true;
-
-            //Read thie TargetFile and return a buffer
-            fs.stat(`${WorkingDirectory}/project/AllUserContent/Sounds/${TargetFile}`, function(err, stats) {
-                fs.open(`${WorkingDirectory}/project/AllUserContent/Sounds/${TargetFile}`, 'r', function(errOpen, fd) {
-                    fs.read(fd, Buffer.alloc(stats.size), 0, stats.size, 0, function(errRead, bytesRead, buffer) {
-                        //Get the samples
-                        wav = new wavefile.WaveFile();
-                        wav.fromBuffer(buffer);
-                        Samples = wav.getSamples()[0];
-                        SongLength = Samples.length;
-                        console.log(chalk.green.bold(`Loaded .wav file! Processing file...`));
-
-                        //Start searching for loop timings
-                        let s = new Set();
-
-                        let Segment = [];
-
-                        let blockSize = 20;
-
-                        for (let i = 0; i < SongLength; i++) {
-                            let sl = Samples.slice(i, i + blockSize);
-                            if (sl.length < blockSize) {
-                                break;
-                            }
-                            let x = sl.join(',');
-                            if (s.has(x)) {
-                                console.log(chalk.green.bold.underline(`Success`));
-                                console.log(i);
-                                console.log('S: ', [...s]);
-                                break;
-                            }
-                            Segment.push(Samples[i]);
-                            s.add(x);
-                        }
-
-                        console.log("Found it: ", Segment);
-
-                    });
-                });
-            });
         }
 
         //Move the output to the correct folders
